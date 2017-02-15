10cdddad3e0463a5474110aecfa4310b87062a4b
Kimserey Lam <kimserey.lam@groomgy.com>
2017-02-15 22:44:02 +0000

    Example

---
+++ Example/Main.fs
@@ -0,0 +1,40 @@
+namespace FormSubmitMime
+
+open System
+open System.IO
+open WebSharper
+open WebSharper.Sitelets
+
+
+module Site =
+    type EndPoint =
+    | [<EndPoint "POST /upload">] Upload
+
+    let Main = Application.MultiPage (fun ctx -> 
+        function Upload -> Content.Text "Received")
+
+module SelfHostedServer =
+
+    open global.Owin
+    open Microsoft.Owin.Hosting
+    open Microsoft.Owin.StaticFiles
+    open Microsoft.Owin.FileSystems
+    open WebSharper.Owin
+
+    [<EntryPoint>]
+    let Main args =
+        let rootDirectory, url =
+            match args with
+            | [| rootDirectory; url |] -> rootDirectory, url
+            | [| url |] -> "..", url
+            | [| |] -> "..", "http://localhost:9000/"
+            | _ -> eprintfn "Usage: FormSubmitMime ROOT_DIRECTORY URL"; exit 1
+        use server = WebApp.Start(url, fun appB ->
+            appB.UseStaticFiles(
+                    StaticFileOptions(
+                        FileSystem = PhysicalFileSystem(rootDirectory)))
+                .UseSitelet(rootDirectory, Site.Main)
+            |> ignore)
+        stdout.WriteLine("Serving {0}", url)
+        stdin.ReadLine() |> ignore
+        0
